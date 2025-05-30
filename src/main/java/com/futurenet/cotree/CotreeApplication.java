4128b01b764f3c9cc585b6a3d230cc7d39d041b2
hyeon8571 <w01085914442@gmail.com>
2025-05-30 12:40:38 +0900

    chore: init spring project

---
+++ src/main/java/com/futurenet/cotree/CotreeApplication.java
@@ -0,0 +1,13 @@
+package com.futurenet.cotree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futurenet/cotree/CotreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurenet.cotree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
