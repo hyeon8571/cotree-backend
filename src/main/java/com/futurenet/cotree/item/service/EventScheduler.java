545b7a8a7af110e9e6b1d84dc9f228cf9d66f7d3
hyeon8571 <w01085914442@gmail.com>
2025-07-08 10:43:50 +0900

    fix: 스케쥴러 시간 변경

--- src/main/java/com/futurenet/cotree/item/service/EventScheduler.java
+++ src/main/java/com/futurenet/cotree/item/service/EventScheduler.java
@@ -23,8 +23,7 @@ public class EventScheduler {
     private final ItemRepository itemRepository;
     private final RedisTemplate<String, String> redisTemplate;
 
-    //@Scheduled(cron = "0 32 10 * * *")
-    @Scheduled(cron = "0 0/3 * * * *")
+    @Scheduled(cron = "0 45 10 * * *")
     @Transactional
     public void registerEventItems() {
 
